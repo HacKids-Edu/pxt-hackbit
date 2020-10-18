e562eb5f14a580662bf8020f2e2d0395cd89d2c6
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-18 12:50:59 -0300

    Update thingspeak.ts

--- thingspeak.ts
+++ thingspeak.ts
@@ -64,6 +64,7 @@ namespace ESP8266ThingSpeak {
     //% block="Upload data to ThingSpeak|URL/IP = %ip|Write API key = %write_api_key|Field 1 = %n1|Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
     //% ip.defl=api.thingspeak.com
     //% write_api_key.defl=your_write_api_key
+    //% subcategory="Thingspeak" color=#851DE8    
     export function connectThingSpeak(ip: string, write_api_key: string, n1: number, n2: number, n3: number, n4: number, n5: number, n6: number, n7: number, n8: number) {
         if (wifi_connected && write_api_key != "") {
             thingspeak_connected = false
@@ -86,6 +87,7 @@ namespace ESP8266ThingSpeak {
     */
     //% block="Wait %delay ms"
     //% delay.min=0 delay.defl=5000
+    //% subcategory="Thingspeak" color=#851DE8    
     export function wait(delay: number) {
         if (delay > 0) basic.pause(delay)
     }
@@ -94,6 +96,7 @@ namespace ESP8266ThingSpeak {
     * Check if ESP8266 successfully connected to Wifi
     */
     //% block="Wifi connected ?"
+    //% subcategory="Thingspeak" color=#851DE8    
     export function isWifiConnected() {
         return wifi_connected
     }
@@ -102,6 +105,7 @@ namespace ESP8266ThingSpeak {
     * Check if ESP8266 successfully connected to ThingSpeak
     */
     //% block="ThingSpeak connected ?"
+    //% subcategory="Thingspeak" color=#851DE8    
     export function isThingSpeakConnected() {
         return thingspeak_connected
     }
@@ -110,6 +114,7 @@ namespace ESP8266ThingSpeak {
     * Check if ESP8266 successfully uploaded data to ThingSpeak
     */
     //% block="Last data upload successful ?"
+    //% subcategory="Thingspeak" color=#851DE8    
     export function isLastUploadSuccessful() {
         return last_upload_successful
     }
