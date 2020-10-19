da2159d899f6cfe55f26e53a460d247221caf87e
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-19 09:57:12 -0300

    Update cubbit.ts

--- cubbit.ts
+++ cubbit.ts
@@ -4,7 +4,7 @@
  * and an LED matrix to express emotions. 
  * Anyone can easily use cub:bit through programing and onboard sensors.
  */
-//% weight=100 color=#00C6F5 icon="\uf5d2" block="Hackbit cub:bit"
+//% color=#00C6F5 icon="\uf5d2" block="Hackbit cubbit"
 namespace cubbit {
     export enum LedLr {
         //% block="A button"
