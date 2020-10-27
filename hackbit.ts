c42f76f331086396021dee989f603bf81cdb82fa
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-27 05:55:56 -0300

    Update hackbit.ts

--- hackbit.ts
+++ hackbit.ts
@@ -204,13 +204,13 @@ namespace hackbit {
         return pins.digitalReadPin(pin);
     }
 
-    /**   Here are suggested sensor values:
-    *       Min  Typ  Max  Condition
-    *       0    0    0    sensor in open air
-    *       0    20   300  sensor in dry soil
-    *       300  580  700  sensor in humid soil
-    *       700  940  950  sensor in water    
-    */
+    //   Here are suggested sensor values:
+    //      Min  Typ  Max  Condition
+    //       0    0    0    sensor in open air
+    //       0    20   300  sensor in dry soil
+    //       300  580  700  sensor in humid soil
+    //       700  940  950  sensor in water    
+    
     /**
     * Get soil moisture(0~100%)
     * @param soilmoisturepin describe parameter here, eg: AnalogPin.P1
