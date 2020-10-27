8643510e74d4e999ddb5185eac1b3f40a163ea45
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-27 08:29:02 -0300

    Update hackbit.ts

--- hackbit.ts
+++ hackbit.ts
@@ -244,6 +244,124 @@ namespace hackbit {
         return Math.round(lightintensity);
     }
 
+    /** 
+    * Get noise(dB)
+    * @param noisepin describe parameter here, eg: AnalogPin.P1
+    */
+    //% blockId="hackbitreadnoise" block="value of noise(dB) at pin %noisepin"
+    //% subcategory=Sensor  group="Analog" color=#04FE33 icon="\uf1eb"    
+    export function ReadNoise(noisepin: AnalogPin): number {
+        let level = 0
+        let voltage = 0
+        let noise = 0
+        let h = 0
+        let l = 0
+        let sumh = 0
+        let suml = 0
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        for (let i = 0; i < 1000; i++) {
+            level = level + pins.analogReadPin(noisepin)
+        }
+        level = level / 1000
+        for (let i = 0; i < 1000; i++) {
+            voltage = pins.analogReadPin(noisepin)
+            if (voltage >= level) {
+                h += 1
+                sumh = sumh + voltage
+            } else {
+                l += 1
+                suml = suml + voltage
+            }
+        }
+        if (h == 0) {
+            sumh = level
+        } else {
+            sumh = sumh / h
+        }
+        if (l == 0) {
+            suml = level
+        } else {
+            suml = suml / l
+        }
+        noise = sumh - suml
+        if (noise <= 4) {
+            noise = pins.map(
+                noise,
+                0,
+                4,
+                30,
+                50
+            )
+        } else if (noise <= 8) {
+            noise = pins.map(
+                noise,
+                4,
+                8,
+                50,
+                55
+            )
+        } else if (noise <= 14) {
+            noise = pins.map(
+                noise,
+                9,
+                14,
+                55,
+                60
+            )
+        } else if (noise <= 32) {
+            noise = pins.map(
+                noise,
+                15,
+                32,
+                60,
+                70
+            )
+        } else if (noise <= 60) {
+            noise = pins.map(
+                noise,
+                33,
+                60,
+                70,
+                75
+            )
+        } else if (noise <= 100) {
+            noise = pins.map(
+                noise,
+                61,
+                100,
+                75,
+                80
+            )
+        } else if (noise <= 150) {
+            noise = pins.map(
+                noise,
+                101,
+                150,
+                80,
+                85
+            )
+        } else if (noise <= 231) {
+            noise = pins.map(
+                noise,
+                151,
+                231,
+                85,
+                90
+            )
+        } else {
+            noise = pins.map(
+                noise,
+                231,
+                1023,
+                90,
+                120
+            )
+        }
+        noise = Math.round(noise)
+        return Math.round(noise)
+    }
+
+
     /**
      * Get analog potentiometer value [0-1023]
     */
