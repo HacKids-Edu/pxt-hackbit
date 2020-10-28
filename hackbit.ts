aeda306ed59a2bbe68e603f178ed27e4a6d39cb1
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-28 19:39:45 -0300

    Update hackbit.ts

--- hackbit.ts
+++ hackbit.ts
@@ -835,8 +835,8 @@ namespace hackbit {
      * Converts the key name to a number
      * Useful for comparisons
      */
-    //% blockId=joystickkey block="%joystickkey"
-    //% joystickkey.fieldEditor="gridpicker" joystickkey.fieldOptions.columns=2        
+    //% blockId=joystickkey block="%jkey"
+    //% jkey.fieldEditor="gridpicker" jkey.fieldOptions.columns=2        
     //% group="Analog"
     //% subcategory=Input color=#851DE8 icon="\uf1eb"
     export function joystickkey(key: GroveJoystickKey): number {
