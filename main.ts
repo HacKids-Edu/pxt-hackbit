1a33791bdd033a4f70ea574987e407678f91c951
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-29 11:12:41 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+/**
+ * Library for hackbit,  grove micro:bit project board https://www.hackids.com.br
+*/
+//% weight=10 color=#FF1344 icon="\uf0eb"    
 namespace hackbit {
 // /**
 //  * Library for hackbit,  grove micro:bit project board https://www.hackids.com.br
