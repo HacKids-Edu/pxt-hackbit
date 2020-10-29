4b79401acf937c537128ba8bd57df2133bb368f1
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-10-29 11:11:35 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-/**
- * Library for hackbit,  grove micro:bit project board https://www.hackids.com.br
-*/
-//% weight=10 color=#FF1344 icon="\uf0eb"    
-
 namespace hackbit {
 // /**
 //  * Library for hackbit,  grove micro:bit project board https://www.hackids.com.br
