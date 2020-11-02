de2b84a63e3bafd6f5fcbb321461fcdce514b3ff
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-02 17:00:30 -0300

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-// test cub:bit
+HackbitLCD.connectLcd(39)
+HackbitLCD.clearLcd1602()
+HackbitLCD.setLcdBacklight(LcdBacklight.Off)
+basic.forever(function () {
+    HackbitLCD.showStringOnLcd1602("Hack:bit", HackbitLCD.position1602(LcdPosition1602.Pos1), 16)
+})
