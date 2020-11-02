17145486460294ebf46ea41ea74848f6e344c361
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-02 17:19:20 -0300

    Update _locales/hackbit-jsdoc-strings.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-// Library for hack:bit - micro:bit project board https://www.hackids.com.br
 basic.forever(function () {
     if (hackbit.buttonLEDState(DigitalPin.P1)) {
         hackbit.ledState(DigitalPin.P2, hackbit.OnOff.On)
