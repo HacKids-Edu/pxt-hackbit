73f0fc6599ec9402f46e2e849790607813174ea5
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-17 11:10:58 -0300

    Update main.blocks, main.ts, hackbit.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 // www.hackids.com.br
 basic.forever(function () {
     serial.writeValue("x", pins.digitalReadPin(DigitalPin.P3))
-    if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+    if (hackbit.MagneticSwitchState(DigitalPin.P3)) {
         music.playTone(262, music.beat(BeatFraction.Whole))
     }
 })
