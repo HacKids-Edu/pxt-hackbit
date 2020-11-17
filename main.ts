4c94544dfcfc89fd1963b90e746b773403de6d68
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-17 10:56:59 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // www.hackids.com.br
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+    if (hackbit.MagneticSwitchState(DigitalPin.P3)) {
         music.playMelody("C5 B A G F E D C ", 120)
     }
 })
