64f32b24a24e6242448b4891226b5d20536279ac
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-17 18:41:08 -0300

    Update main.blocks, _locales/pt-BR/hackbit-strings.json, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     strip.showRainbow(1, 360)
     strip.show()
-    basic.pause(500)
+    basic.pause(1000)
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
     strip.show()
-    basic.pause(500)
+    basic.pause(1000)
 })
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P1, 10, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P2, 10, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
