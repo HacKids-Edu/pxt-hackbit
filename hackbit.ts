03fd8c8caa5621541ed60e905c91b59dfc75bbae
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-11-17 20:40:43 -0300

    Update main.blocks, main.ts, hackbit.ts

--- hackbit.ts
+++ hackbit.ts
@@ -550,7 +550,7 @@ namespace hackbit {
 
     /**
     * Get dht11 temperature and humidity Value
-    * @param dht11pin describe parameter here, eg: DigitalPin.P15     
+    * @param dht11pin describe parameter here, eg: DigitalPin.P12     
     */
     //% blockId="hackbitDHT11Read" block="DHT11 sensor %dht11pin %dht11type value"
     //% dht11pin.fieldEditor="gridpicker"
@@ -565,37 +565,68 @@ namespace hackbit {
         basic.pause(18)
         let i = pins.digitalReadPin(dht11pin)
         pins.setPull(dht11pin, PinPullMode.PullUp);
-
-        while (pins.digitalReadPin(dht11pin) == 1);
-        while (pins.digitalReadPin(dht11pin) == 0);
-        while (pins.digitalReadPin(dht11pin) == 1);
-
-        let value = 0;
-        let counter = 0;
-
-        for (let i = 0; i <= 32 - 1; i++) {
-            while (pins.digitalReadPin(dht11pin) == 0);
-            counter = 0
-            while (pins.digitalReadPin(dht11pin) == 1) {
-                counter += 1;
-            }
-            if (counter > 4) {
-                value = value + (1 << (31 - i));
-            }
-        }
-
         switch (dht11type) {
             case 0:
-                return (value & 0x0000ff00) >> 8
+                let dhtvalue1 = 0;
+                let dhtcounter1 = 0;
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter1 = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter1 += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter1 > 2) {
+                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
+                        }
+                    }
+                }
+                return ((dhtvalue1 & 0x0000ff00) >> 8);
                 break;
             case 1:
-                return ((value & 0x0000ff00) >> 8) * 9 / 5 + 32
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+                let dhtvalue = 0;
+                let dhtcounter = 0;
+                for (let i = 0; i <= 32 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    dhtcounter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        dhtcounter += 1;
+                    }
+                    if (i > 15) {
+                        if (dhtcounter > 2) {
+                            dhtvalue = dhtvalue + (1 << (31 - i));
+                        }
+                    }
+                }
+                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
                 break;
             case 2:
-                return value >> 24
-                break;
+                while (pins.digitalReadPin(dht11pin) == 1);
+                while (pins.digitalReadPin(dht11pin) == 0);
+                while (pins.digitalReadPin(dht11pin) == 1);
+
+                let value = 0;
+                let counter = 0;
+
+                for (let i = 0; i <= 8 - 1; i++) {
+                    while (pins.digitalReadPin(dht11pin) == 0);
+                    counter = 0
+                    while (pins.digitalReadPin(dht11pin) == 1) {
+                        counter += 1;
+                    }
+                    if (counter > 3) {
+                        value = value + (1 << (7 - i));
+                    }
+                }
+                return value;
             default:
-                return 0
+                return 0;
         }
     }
 
--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 basic.forever(function () {
-    basic.showNumber(hackbit.dht11Sensor(DigitalPin.P1, hackbit.DHT11Type.DHT11_temperature_C))
+    basic.showNumber(hackbit.dht11Sensor(DigitalPin.P12, hackbit.DHT11Type.DHT11_temperature_C))
 })
