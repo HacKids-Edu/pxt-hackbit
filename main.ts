11a0bc86f4fa81a2db3a57a9b19e4473a4611da9
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2020-12-23 16:38:59 -0300

    Update main.blocks, _locales/hackbit-jsdoc-strings.json, _locales/pt-BR/hackbit-jsdoc-strings.json, main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,6 @@ input.onButtonPressed(Button.A, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-	
+    hackbitmotors.GeekServo360(hackbitmotors.Servos.S1, hackbitmotors.enPos.forward, 0)
+    hackbitmotors.GeekServo(hackbitmotors.Servos.S1, 0)
 })
