f788afb4246b8231d17a9114510bd24545fc1db4
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2020-12-23 09:34:55 -0300

    Update main.blocks, _locales/pt-BR/hackbit-strings.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+input.onButtonPressed(Button.A, function () {
+    hackbitmotors.StepperDegree(hackbitmotors.Steppers.M1, -360)
+})
+input.onButtonPressed(Button.B, function () {
+    hackbitmotors.MotorStopAll()
+})
