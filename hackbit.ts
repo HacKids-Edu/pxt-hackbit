20bb62186f2ceebd28678887b69a6028b3c7c4b3
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2020-12-28 18:10:07 -0300

    Update hackbit.ts

--- hackbit.ts
+++ hackbit.ts
@@ -131,17 +131,6 @@ namespace hackbit {
         fahrenheit,
     }
 
-    export enum DHT11Type {
-        //% block="temperature(℃)" enumval=0
-        DHT11_temperature_C,
-
-        //% block="temperature(℉)" enumval=1
-        DHT11_temperature_F,
-
-        //% block="humidity(0~100)" enumval=2
-        DHT11_humidity,
-    }
-
     export let _temperature: number = -999.0
     export let _humidity: number = -999.0
     export let _temptype: tempType = tempType.celsius
@@ -573,88 +562,6 @@ namespace hackbit {
             15
         );
         return Math.round(UVlevel)
-    }
-
-    /**
-    * Get dht11 temperature and humidity Value
-    * @param dht11pin describe parameter here, eg: DigitalPin.P12     
-    */
-    //% blockId="hackbitDHT11Read" block="DHT11 sensor %dht11pin %dht11type value"
-    //% dht11pin.fieldEditor="gridpicker"
-    //% dht11pin.fieldOptions.columns=3
-    //% dht11type.fieldEditor="gridpicker"
-    //% dht11type.fieldOptions.columns=2
-    //% dht11type.min=0 dht11type.max=1
-    //% subcategory=Sensor  group="Analog" 
-    //% color=#D84A51 
-    export function dht11value(dht11pin: DigitalPin, dht11type: DHT11Type): number {
-        pins.digitalWritePin(dht11pin, 0)
-        basic.pause(18)
-        let i = pins.digitalReadPin(dht11pin)
-        pins.setPull(dht11pin, PinPullMode.PullUp);
-        switch (dht11type) {
-            case 0:
-                let dhtvalue1 = 0;
-                let dhtcounter1 = 0;
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
-                for (let i = 0; i <= 32 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    dhtcounter1 = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        dhtcounter1 += 1;
-                    }
-                    if (i > 15) {
-                        if (dhtcounter1 > 2) {
-                            dhtvalue1 = dhtvalue1 + (1 << (31 - i));
-                        }
-                    }
-                }
-                return ((dhtvalue1 & 0x0000ff00) >> 8);
-                break;
-            case 1:
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
-                let dhtvalue = 0;
-                let dhtcounter = 0;
-                for (let i = 0; i <= 32 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    dhtcounter = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        dhtcounter += 1;
-                    }
-                    if (i > 15) {
-                        if (dhtcounter > 2) {
-                            dhtvalue = dhtvalue + (1 << (31 - i));
-                        }
-                    }
-                }
-                return Math.round((((dhtvalue & 0x0000ff00) >> 8) * 9 / 5) + 32);
-                break;
-            case 2:
-                while (pins.digitalReadPin(dht11pin) == 1);
-                while (pins.digitalReadPin(dht11pin) == 0);
-                while (pins.digitalReadPin(dht11pin) == 1);
-
-                let value = 0;
-                let counter = 0;
-
-                for (let i = 0; i <= 8 - 1; i++) {
-                    while (pins.digitalReadPin(dht11pin) == 0);
-                    counter = 0
-                    while (pins.digitalReadPin(dht11pin) == 1) {
-                        counter += 1;
-                    }
-                    if (counter > 3) {
-                        value = value + (1 << (7 - i));
-                    }
-                }
-                return value;
-            default:
-                return 0;
-        }
     }
 
     /** 
