bab66d57eb436f93f6f8fb206721ac7b222e6b88
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2020-12-28 18:08:58 -0300

    Update main.blocks, main.ts, hackbit.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+hackbit.selectTempType(hackbit.tempType.celsius)
 basic.forever(function () {
-	
+    hackbit.queryData(
+    hackbit.DHTtype.DHT11,
+    DigitalPin.P0,
+    true,
+    false,
+    true
+    )
 })
