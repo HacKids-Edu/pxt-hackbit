727f2464bd5d22064727c9b458f5762696ff7fb7
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-01-05 14:49:45 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-basic.forever(function () {
-    serial.writeValue("x", pins.digitalReadPin(DigitalPin.P2))
-    if (hackbit.WaterState(DigitalPin.P2)) {
-        basic.showIcon(IconNames.Asleep)
-    } else {
-        basic.showIcon(IconNames.Heart)
-    }
-})
+
