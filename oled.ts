c4c713f02da53d81f4ebd1b0e5b17d03625c38ca
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-09-24 12:04:09 -0300

    Atualizar 'oled.ts, oledplus.ts'

--- oled.ts
+++ oled.ts
@@ -5,7 +5,7 @@ declare interface Math {
 /**
 * Functions to hack:bit board by Hackids 
 */
-//% weight=10 color=#F67D54 icon="\uf2c2" block="Hackbit OLED" blockId="HackbitOLEDDisplay"
+//% weight=10 color=#f68f54 icon="\uf26c" block="Hackbit OLED" blockId="HackbitOLEDDisplay"
 //% groups="OLED"
 
 namespace HackbitOLEDDisplay {
