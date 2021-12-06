c17930a5fdb57e1069ed2b793b2e996d241c5668
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-12-06 09:53:55 -0300

    Atualizar 'hackbitsensors.ts'

--- hackbitsensors.ts
+++ hackbitsensors.ts
@@ -1,4 +1,4 @@
-//% weight=51 color=#C82A32 icon="\uf0eb" block="Hackbit Sensors"
+//% weight=10 color=#C82A32 icon="\uf0eb" block="Hackbit Sensors"
 namespace hackbitsensor {
     export enum PingUnit {
         //% block="cm",
