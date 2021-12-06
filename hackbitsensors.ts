b33145c31028f3eed69a4fec503453a546524234
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-12-06 10:40:14 -0300

    Atualizar 'hackbitsensors.ts'

--- hackbitsensors.ts
+++ hackbitsensors.ts
@@ -1559,7 +1559,7 @@ namespace hackbitsensor {
     //% block="round $value decimal $decimal"
     //% block.loc.pt-BR="arredondar $value decimal $decimal"
     //% decimal.defl=2
-    //% subcategory=Math
+    //% subcategory=Maths
     //% color=#600DA3
     //% advanced=false
     export function roundwithprecision(value: number, decimal: number): number {
