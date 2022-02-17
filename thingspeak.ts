65df288d63efe928a74cd9f736312e4d8600341d
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2022-02-17 17:45:01 -0300

    Atualizar 'thingspeak.ts'

--- thingspeak.ts
+++ thingspeak.ts
@@ -8,57 +8,21 @@ namespace ESP8266ThingSpeak {
     let thingspeak_connected: boolean = false
     let last_upload_successful: boolean = false
 
-    // ThingSpeak
+    let array_keys: Array<string> = []
+    let array_values: Array<string> = []
+    let httpReturnString: string = ""
+    let HTTP_received: (Error_code: string, Data: string) => void = null;
 
-    // write AT command with CR+LF ending
-    function sendAT(command: string, wait: number = 100) {
-        serial.writeString(command + "\u000D\u000A")
-        basic.pause(wait)
-    }
 
-    // wait for certain response from ESP8266
-    function waitResponse(): boolean {
-        let serial_str: string = ""
-        let result: boolean = false
-        let time: number = input.runningTime()
-        while (true) {
-            serial_str += serial.readString()
-            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
-            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
-                result = true
-                break
-            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
-                break
-            }
-            if (input.runningTime() - time > 30000) break
-        }
-        return result
-    }
-
-    // IFTTT
-
-    function sendAtCmd(cmd: string) {
-        serial.writeString(cmd + "\u000D\u000A")
+    export enum httpMethod {
+        //% block="GET"
+        GET,
+        //% block="POST"
+        POST
     }
 
 
-    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
-        let buffer = ""
-        let start = input.runningTime()
-
-        while ((input.runningTime() - start) < timeout) {
-            buffer += serial.readString()
-
-            if (buffer.includes(target1)) return 1
-            if (buffer.includes(target2)) return 2
-            if (buffer.includes(target3)) return 3
-
-            basic.pause(100)
-        }
-
-        return 0
-    }
-
+    // ------- WiFi -------
 
     /**
     * Initialize ESP8266 module and connect it to Wifi router
@@ -89,6 +53,32 @@ namespace ESP8266ThingSpeak {
         sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
         wifi_connected = waitResponse()
         basic.pause(100)
+    }
+
+    // ------- ThingSpeak -------
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 100) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200) serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED")) {
+                result = true
+                break
+            } else if (serial_str.includes("ERROR") || serial_str.includes("SEND FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 30000) break
+        }
+        return result
     }
 
     /**
@@ -158,6 +148,28 @@ namespace ESP8266ThingSpeak {
         return last_upload_successful
     }
 
+    // ------- IFTTT -------
+    function sendAtCmd(cmd: string) {
+        serial.writeString(cmd + "\u000D\u000A")
+    }
+
+    function waitAtResponse(target1: string, target2: string, target3: string, timeout: number) {
+        let buffer = ""
+        let start = input.runningTime()
+
+        while ((input.runningTime() - start) < timeout) {
+            buffer += serial.readString()
+
+            if (buffer.includes(target1)) return 1
+            if (buffer.includes(target2)) return 2
+            if (buffer.includes(target3)) return 3
+
+            basic.pause(100)
+        }
+
+        return 0
+    }
+
     /**
      * Send data to IFTTT
      */
@@ -212,6 +224,114 @@ namespace ESP8266ThingSpeak {
 
             if (result == 1) break
         }
+    }
+
+    // ------- HTTP -------
+    /**
+     * Use hack:bit & ESP8266 to send the HTTP request, input the URL of your API.
+     * The Body content only available for POST method.
+     * The POST Body Content-Type was "application/json",
+     * DO NOT include "&" symbol in the JSON content.
+     * 
+     */
+    //%subcategory="IoT Service"
+    //%blockId=wifi_ext_board_generic_http
+    //% block="Send HTTP Request |Method %method|URL:%url|Body:%body"
+    //% weight=115	 group="HTTP" 
+    //% inlineInputMode=external
+    export function sendGenericHttp(method: httpMethod, url: string, body: string): void {
+        //httpReturnArray = []
+        let temp = ""
+        switch (method) {
+            case httpMethod.GET:
+                temp = "GET"
+                break
+            case httpMethod.POST:
+                temp = "POST"
+                break
+        }
+        serial.writeLine("(AT+http?method=" + temp + "&url=" + url + "&header=" + "&body=" + body + ")");
+    }
+
+    /**
+     * After sending the HTTP request, the response will be return to this handler, you may access the http stauts code and the return body.
+     */
+
+    //%subcategory="IoT Service"
+    //% blockId="wifi_ext_board_http_receive" 
+    //% block="On HTTP received"	 group="HTTP"
+    //% weight=108 draggableParameters=reporter
+    //% blockGap=20
+
+    export function on_HTTP_recevid(handler: (HTTP_Status_Code: string, Data: string) => void): void {
+        HTTP_received = handler;
+    }
+
+    /**
+     * This function can extract the value of specific key from a JSON format String.
+     * Fill in the Key field that you are searching from json_object, then put the source into the Source placeholder(e.g HTTP return Data).
+     * It will search the key from Source string and return the corresponding value.
+     * When using at the mulit-level JSON, you need to use this function several time to extract the value one by one level.
+     * @param target Key that looking for
+     * @param source Source string that to be extract from
+     */
+    //%subcategory="IoT Service"
+    //% blockId="JSON_extractor"
+    //%block="Get value of Key %target from JSON String %source"
+    //% weight=107 group="HTTP"
+    export function get_value(target: string, source: string): string {
+
+        //clear the keys & values array
+        array_keys = []
+        array_values = []
+        //prase the JSON String to Object
+        let json_object = JSON.parse(source)
+        //Get the count of keys for the For-Loop to run
+        let total_keys = Object.keys(json_object).length
+        // Start work on each keys
+        for (let i = 0; i < total_keys; i++) {
+            //Push each key from JSON Object to keys array
+            array_keys.push(Object.keys(json_object)[i])
+            // Check the corresponding value of the key from Object, 
+            // if it is string or number type, push it to value array as normal
+            if ((typeof (json_object[array_keys[array_keys.length - 1]]) == "string") || (typeof (json_object[array_keys[array_keys.length - 1]]) == "number")) {
+                //push the string or number value to array
+                array_values.push(json_object[array_keys[array_keys.length - 1]])
+
+            }
+            // if the value is a Object type, mostly is next level JSON object
+            else if (typeof (json_object[array_keys[array_keys.length - 1]]) == "object") {
+                //Use stringify to convert it back to string, allow to return the stringify object to user,
+                //User can perform JSON prase function again later, while the source can set as this return string
+                array_values.push(JSON.stringify(json_object[array_keys[array_keys.length - 1]]))
+
+            }
+        }
+        //After input all the data, search the target's key index
+        let target_index = array_keys.indexOf(target)
+        //Return the value of that key
+        return array_values[target_index]
+    }
+
+    //% subcategory="IoT Service"
+    //% blockId="wifi_ext_board_generic_http_array_return" 
+
+    //% block="HTTP response (string array)"
+    //% weight=110	 group="HTTP"
+    //% blockGap=7
+    //% blockHidden=true
+    export function getGenericHttpReturn(): Array<string> {
+        return [""];
+    }
+    //% subcategory="IoT Service"
+    //% blockId="wifi_ext_board_generic_http_return" 
+    //% block="HTTP response (string)"
+    //% weight=110	 group="HTTP"
+    //% blockGap=7
+    //% blockHidden=true
+    export function getHttpReturn(): string {
+        return httpReturnString;
+
     }
 
 }
